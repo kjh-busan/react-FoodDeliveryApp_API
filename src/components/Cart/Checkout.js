bbc1c5fa6d573a22e2af93895f891bef052c600f
kjh-busan <picsmash7@gmail.com>
2023-04-06 01:00:01 +0900

    feat: form valid WIP2

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,32 +1,34 @@
-import React from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
   const confirmHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <form onSubmit={confirmHandler}>
-      <div className={classes.contorl}>
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" placeholder="Name plz" />
+        <input type="text" id="name" />
       </div>
-      <div className={classes.contorl}>
+      <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" placeholder="Street! plz" />
+        <input type="text" id="street" />
       </div>
-      <div className={classes.contorl}>
+      <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" placeholder="Postal! plz" />
+        <input type="text" id="postal" />
       </div>
-      <div className={classes.contorl}>
+      <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" placeholder="City! plz" />
+        <input type="text" id="city" />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
